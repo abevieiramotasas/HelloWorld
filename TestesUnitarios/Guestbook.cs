52f96d55f64091f91a79e00370545289979edb9a
Abelardo <abbbekgb@gmail.com>
2012-09-09 12:42:33 -0300

    Terminando por hoje, refatorando os testes :)

--- TestesUnitarios/Guestbook.cs
+++ TestesUnitarios/Guestbook.cs
@@ -9,14 +9,27 @@ namespace TestesUnitarios
     [TestFixture]
     public class Guestbook
     {
+        private HomeController controller;
         [TestCase]
         public void Index()
         {
-            HomeController controller = new HomeController();
-
             ViewResult result = controller.Index() as ViewResult;
 
             Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", (string) result.ViewBag.Message);
+        }
+
+        [TestCase]
+        public void About()
+        {
+            ViewResult result = controller.About() as ViewResult;
+
+            Assert.NotNull(result);
+        }
+
+        [SetUp]
+        public void Up()
+        {
+            this.controller = new HomeController();
         }
     }
 }
