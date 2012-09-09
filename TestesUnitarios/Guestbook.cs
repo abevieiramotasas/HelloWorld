f5345aff7ebd5a04b39a193e6503c9ea07be275b
Abelardo <abbbekgb@gmail.com>
2012-09-09 12:33:22 -0300

    deletado(finalmente hehe) o projeto de teste do vs e adicionado projeto nunit

---
+++ TestesUnitarios/Guestbook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using HelloWorld.Controllers;
+using System.Web.Mvc;
+using System.Web;
+
+namespace TestesUnitarios
+{
+    [TestFixture]
+    public class Guestbook
+    {
+        [TestCase]
+        public void Index()
+        {
+            HomeController controller = new HomeController();
+
+            ViewResult result = controller.Index() as ViewResult;
+
+            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", (string) result.ViewBag.Message);
+        }
+    }
+}
--- TestesUnitarios/Properties/AssemblyInfo.cs
+++ TestesUnitarios/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HelloWorld.Tests")]
+[assembly: AssemblyTitle("TestesUnitarios")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("HelloWorld.Tests")]
+[assembly: AssemblyProduct("TestesUnitarios")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("03004295-ae40-4f13-90ac-497e0ff6ea57")]
+[assembly: Guid("b157ff87-f77d-4c71-8b6c-98a907ddbde8")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
